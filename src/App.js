35c2f7dcee5a92cd6984838cd0cdd48cb36dffc8
Vampireshadow <41458199+Vampireshadow@users.noreply.github.com>
2022-12-08 14:48:14 -0600

    Update App.js

--- src/App.js
+++ src/App.js
@@ -3,8 +3,36 @@ import React from 'react'
 function App() {
   return (
     <div className="container mt-5">
-      <h1>Tarea </h1>
+      <h1>Tareas </h1>
       <hr/>
+      <div className="row">
+        <div className="col-8">
+          <h4 className="texs-center">Lista de Treas</h4>
+          <ul className="list-group">
+           <li className="list-group-item">
+             <span className="lead">Nombre de la tarea</span>
+             <button className="btn btn-danger btn-sm float-right mx-2">Eliminar</button>
+             <button className="btn btn-warning btn-sm float-right">Editar</button>
+           </li>
+          </ul>
+        </div>
+        <div className="col-4">
+          <h4 className="texs-center">Formulario</h4>
+          <form>
+            <input
+             type="text"
+             className="from-control mb-2"
+             placeholder="Ingrese la tarea . . ."
+            />
+            <button 
+            className="btn btn-dark btn-block"
+            type="submit"
+            >
+              Agregar
+            </button>
+          </form>
+        </div>
+      </div>
     </div>
   )
 }
